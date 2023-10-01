9963518b616bad97e8ad6f644c7beeb7e8c2c085
KobeLemon <kobe42lemon@outlook.com>
2023-10-01 02:40:25 -0600

    Fixed homepage & swagger.json's host & schemes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
+let swaggerHostLink = require('../swagger.json');
+let pageLink = '';
+
+if (swaggerHostLink.host == 'localhost:3000') {
+    pageLink = 'http://localhost:3000';
+} else if (swaggerHostLink.host == 'cse341-project2-kl.onrender.com' || swaggerHostLink.host == 'cse341-project2-kl.onrender.com/') {
+    pageLink = 'https://cse341-project2-kl.onrender.com';
+} else {
+    pageLink = `<strong>Invalid Host Address:</strong> ${swaggerHostLink.host}`;
+    console.log(pageLink);
+}
 
 router.use('/', require('./swagger'));
 
+const homePageMsg = 
+    `<p>This API is a mockup of a database showing fake filler employee information and fake filler customer information. Swagger UI is used to perform the CRUD operations.</p>
+    <p>To access all employees, visit this site: <a href="${pageLink}/employees">${pageLink}/employees</a></p>
+    <p>To access all customers, visit this site: <a href="${pageLink}/customers">${pageLink}/customers</a></p>
+    <p>To access Swagger UI for the CRUD functions, visit this site: <a href="${pageLink}/api-docs">${pageLink}/api-docs</a></p>`
+
+
 router.get('/', (req, res) => (
-    res.send('This API is a mockup of a database showing fake filler employee information and fake filler customer information.')
+    res.send(homePageMsg)
 ));
 
 router.use('/employees', require('./employees'));
